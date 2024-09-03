014b5755c8eb0ab25bf1611e638af003c8c0bfdd
Adithya K B <adithyakb93@gmail.com>
2024-09-03 18:26:39 +0530

    feat: Implement player logic and finish game menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { appWindow } from "@tauri-apps/api/window";
 import kaplay from "kaplay";
 import makeBackground from "./utils";
 import { SCALE_FACTOR } from "./constants";
+import makePlayer from "./player";
 
 const k = kaplay({
   width: 1280,
@@ -57,6 +58,36 @@ k.scene("start", async () => {
   });
 
   map.add([k.sprite("obstacles"), k.pos()]);
+
+  const player = k.add(makePlayer(k));
+  player.pos = k.vec2(k.center().x - 350, k.center().y + 56);
+
+  const playBtn = k.add([
+    k.rect(200, 50, { radius: 3 }),
+    k.color(k.Color.fromHex("#14638e")),
+    k.area(),
+    k.anchor("center"),
+    k.pos(k.center).x + 30,
+    k.center().y + 60,
+  ]);
+
+  playBtn.add([
+    k.text("Play", { size: 24 }),
+    k.color(k.Color.fromHex("#d7f2f7")),
+    k.area(),
+    k.anchor("center"),
+  ]);
+
+  const goToGame = () => {
+    k.play("confirm");
+    k.go("main");
+  };
+
+  playBtn.onClick(goToGame);
+
+  k.onKeyPress("space", goToGame);
+
+  k.onGamepadButtonPress("south", goToGame);
 });
 
 k.scene("main", async () => {});
